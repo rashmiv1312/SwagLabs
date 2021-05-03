3eddde2915abb9ab9a24de1087f386daac12fa46
rashmi <rashmiv1312@gmail.com>
2021-05-03 23:42:52 +0200

    added code comments

--- src/test/java/no/sampletest/SwagDemoTest.java
+++ src/test/java/no/sampletest/SwagDemoTest.java
@@ -13,10 +13,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
+/**
+ * Selenium Test cases for Swag Labs Demo page
+ * @author Rashmi
+ */
 public class SwagDemoTest {
 
 	private WebDriver driver;
+	private static final String URL = "https://www.saucedemo.com/";
 
 	@Before
 	public void setUp() {
@@ -24,10 +28,17 @@ public class SwagDemoTest {
 		driver = new ChromeDriver();
 	}
 
+	/*
+	 * Test Case Scenario 1:
+	 * Login using the following credentials and check (verify with assert) that an
+	 * error message has been displayed. Use the credentials listed in the next
+	 * bullet point. • Credentials: Username -> locked_out_user , Password ->
+	 * secret_sauce
+	 */
 	@Test
-	public void testLoginFailed() {
+	public void testLockedOutUser() {
 		System.out.println("######## Test Case1 ######");
-		driver.get("https://www.saucedemo.com/");
+		driver.get(URL);
 		WebElement username = driver.findElement(By.id("user-name"));
 		WebElement password = driver.findElement(By.id("password"));
 		WebElement login = driver.findElement(By.xpath("//input[@id='login-button']"));
@@ -40,17 +51,20 @@ public class SwagDemoTest {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 		WebElement loginErrorMsg = driver.findElement(By.xpath("//h3[@data-test='error']"));
-		System.out.println(loginErrorMsg.getText());
 
 		String actualErrorMsg = "Epic sadface: Username and password do not match any user in this service";
 		String expectedErrorMsg = loginErrorMsg.getText();
 		assertEquals(expectedErrorMsg, actualErrorMsg);
 	}
 
+	/*
+	 * Test Case Scenario 2:
+	 * Login using the following credentials (Username ->standard_user , Password -> secret_sauce)
+	 */
 	@Test
-	public void testLoginSuccess() {
+	public void testStandardUser() {
 		System.out.println("######## Test Case2 #######");
-		driver.get("https://www.saucedemo.com/");
+		driver.get(URL);
 		WebElement username = driver.findElement(By.id("user-name"));
 		WebElement password = driver.findElement(By.id("password"));
 		WebElement login = driver.findElement(By.id("login-button"));
@@ -58,26 +72,24 @@ public class SwagDemoTest {
 		username.sendKeys("standard_user");
 		password.sendKeys("secret_sauce");
 		login.click();
-
+		//• Sort the Items using the Price filter (Highest to Lowest)
 		final Select selectBox = new Select(driver.findElement(By.xpath("//select[@class='product_sort_container']")));
 		selectBox.selectByValue("hilo");
-
-		WebElement actualCostForFleeceJacketElement = driver.findElement(By.xpath(
-				"//div[text()='Sauce Labs Fleece Jacket']/ancestor::div[@class='inventory_item_label']/following-sibling::div[@class='pricebar']/div[@class='inventory_item_price']"));
-		String actualCostForFleeceJacket = actualCostForFleeceJacketElement.getText();
-		System.out.println(actualCostForFleeceJacket);
-
-		WebElement actualCostForBackpackElement = driver.findElement(By.xpath(
-				"//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item_label']/following-sibling::div[@class='pricebar']/div[@class='inventory_item_price']"));
-		String actualCostForBackpack = actualCostForBackpackElement.getText();
-		System.out.println(actualCostForBackpack);
-
+		
+		//• Check that the price of the “Sauce Labs Fleece Jacket” is 49 dollars
+		driver.findElement(By.xpath(
+				"//div[text()='Sauce Labs Fleece Jacket']/ancestor::div[@class='inventory_item_label']/following-sibling::div[@class='pricebar']/div[contains(string(),'$49')]"));
+		
+		//• Check that the price of the “Sauce Labs Backpack” is 29 dollars
+		driver.findElement(By.xpath(
+				"//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item_label']/following-sibling::div[@class='pricebar']/div[contains(string(),'$29')]"));
+		
+		//• Add the “Sauce Labs Backpack” to the basket
 		WebElement jacketToCartElement = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
-
 		jacketToCartElement.click();
 
+		//Add the “Sauce Labs Fleece Jacket” to the basket
 		WebElement backPackToCartElement = driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket"));
-
 		backPackToCartElement.click();
 
 		WebElement clickCartElement = driver
@@ -86,18 +98,18 @@ public class SwagDemoTest {
 
 		WebElement verifyCartJacket = driver.findElement((By.xpath(
 				"//div[@class='cart_list']/div[@class='cart_item']/div[@class='cart_item_label']/a/div[text()='Sauce Labs Fleece Jacket']")));
-		System.out.println(verifyCartJacket.getText());
-
-		String product1 = "Sauce Labs Fleece Jacket";
-		String product2 = "Sauce Labs Backpack";
+//		System.out.println(verifyCartJacket.getText());
 
 		WebElement verifyCartbackpack = driver.findElement((By.xpath(
 				"//div[@class='cart_list']/div[@class='cart_item']/div[@class='cart_item_label']/a/div[text()='Sauce Labs Backpack']")));
-		System.out.println(verifyCartbackpack.getText());
+//		System.out.println(verifyCartbackpack.getText());
 
-		assertEquals(product1, verifyCartJacket.getText());
-		assertEquals(product2, verifyCartbackpack.getText());
+		//• Verify that the “Sauce labs fleece jacket” is in the cart (Verify with assert)
+		assertEquals("Sauce Labs Fleece Jacket", verifyCartJacket.getText());
+		//• Verify that the “Sauce labs backpack” is in the cart (Verify with assert)
+		assertEquals("Sauce Labs Backpack", verifyCartbackpack.getText());
 
+		//• Checkout and enter in your information in the available fields.
 		WebElement checkoutButtonElement = driver.findElement(By.id("checkout"));
 		checkoutButtonElement.click();
 
@@ -112,23 +124,26 @@ public class SwagDemoTest {
 
 		WebElement continueButtonElement = driver.findElement(By.id("continue"));
 		continueButtonElement.click();
-
+		
+		//Verify that the Payment information is “SauceCard #31337” (Verify with assert)
 		WebElement paymentInfoElement = driver.findElement(By.xpath("//div[text()='SauceCard #31337']"));
 		assertEquals("SauceCard #31337", paymentInfoElement.getText());
 
+		//Verify that the shipping information is “FREE PONY EXPRESS DELIVERY!” (Verify with assert)
 		WebElement shippingElement = driver.findElement(By.xpath("//div[text()='FREE PONY EXPRESS DELIVERY!']"));
 		assertEquals("FREE PONY EXPRESS DELIVERY!", shippingElement.getText());
 		
-		//xpath with substring function
-//			WebElement totalElement = driver.findElement(By.xpath("substring-after(//div[@class='summary_total_label'],':')"));
+		//• Verify that the total is $86.38 (Verify with assert)
 		WebElement totalElement = driver.findElement(By.xpath("//div[@class='summary_total_label']"));
 		String total = totalElement.getText();
 		int index = total.indexOf('$');
 		assertEquals("$86.38", total.substring(index));
-
+		
+		//• Click on the Finish button.
 		WebElement finishButtonElement = driver.findElement(By.id("finish"));
 		finishButtonElement.click();
-
+		
+		//Verify Order confirmation page
 		WebElement confirmOrderElement = driver.findElement(By.xpath("//h2[@class='complete-header']"));
 		assertEquals("THANK YOU FOR YOUR ORDER", confirmOrderElement.getText());
 
@@ -136,7 +151,8 @@ public class SwagDemoTest {
 		logoutMenuElement.click();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+		
+		//logout the application
 		WebElement logoutElement = driver.findElement(By.xpath("//*[@id='logout_sidebar_link']"));
 		logoutElement.click();
 	}
